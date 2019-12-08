053a187d9d5f9676192078be10c9aba7e8262dfe
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2019-12-08 08:41:55 +0100

    Add day 8

---
+++ src/8/1.ts
@@ -0,0 +1,33 @@
+import * as fs from "fs";
+
+const input: number[] = fs
+  .readFileSync("input", "utf8")
+  .trim()
+  .split("")
+  .map((n: string) => Number(n));
+
+const height = 6;
+const width = 25;
+const pixels = height * width;
+const amountOfLayers = input.length / pixels;
+
+const layers: number[][] = new Array(amountOfLayers);
+for (let i = 0; i < layers.length; i++) {
+  layers[i] = [];
+}
+
+for (let i = 0; i < input.length; i++) {
+  layers[Math.floor(i/pixels)].push(input[i]);
+}
+
+let currentBest = Infinity;
+let currentLayer: number[] = [];
+for (const layer of layers) {
+  const nbOfZeroes = layer.filter(val => val === 0).length;
+  if (nbOfZeroes < currentBest) {
+    currentBest = nbOfZeroes;
+    currentLayer = layer;
+  }
+}
+
+console.log(currentLayer.filter(val => val === 1).length * currentLayer.filter(val => val === 2).length)
---
+++ src/8/2.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+
+const input: number[] = fs
+  .readFileSync("input", "utf8")
+  .trim()
+  .split("")
+  .map((n: string) => Number(n));
+
+const height = 6;
+const width = 25;
+const pixels = height * width;
+const amountOfLayers = input.length / pixels;
+
+const layers: number[][] = new Array(amountOfLayers);
+for (let i = 0; i < layers.length; i++) {
+  layers[i] = [];
+}
+
+for (let i = 0; i < input.length; i++) {
+  layers[Math.floor(i/pixels)].push(input[i]);
+}
+
+function getPixel(pos: number): number {
+  for (let i = 0; i < layers.length; i++) {
+    if (layers[i][pos] !== 2) {
+      return layers[i][pos];
+    }
+  }
+  return 2;
+}
+
+const result: number[] = [];
+for (let i = 0; i < pixels; i++) {
+  result.push(getPixel(i));
+}
+for (let i = 0; i < height; i++) {
+  console.log(result.slice(i*width, (i+1) * width).join(''))
+}
