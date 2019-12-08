cfed2a45cbe0cdcda48ec411290eba766383c878
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2019-12-08 01:39:51 +0100

    Rewrite day 7 to use extracted intcode

--- src/7/1.ts
+++ src/7/1.ts
@@ -1,148 +1,10 @@
 import * as fs from "fs";
+import { intcode } from "../shared/intcode";
 
 const input = fs
   .readFileSync("input", "utf8")
   .split(",")
   .map(Number);
-
-function run(programBase: number[], inputGenerator: Generator<number, number, void>): number {
-  const program = [...programBase];
-  let instructionPointer: number = 0;
-  let lastOutput: number = 0;
-
-  while (instructionPointer >= 0) {
-    const operation = program[instructionPointer] % 100;
-    const modes = String(Math.floor(program[instructionPointer] / 100))
-      .split("")
-      .reverse()
-      .map(Number)
-      .concat(0, 0, 0, 0);
-
-    switch (operation) {
-      case 1:
-        add(modes);
-        break;
-      case 2:
-        multiply(modes);
-        break;
-      case 3:
-        input();
-        break;
-      case 4:
-        output(modes);
-        break;
-      case 5:
-        jumpIf(true, modes);
-        break;
-      case 6:
-        jumpIf(false, modes);
-        break;
-      case 7:
-        check((a, b) => a < b, modes);
-        break;
-      case 8:
-        check((a, b) => a === b, modes);
-        break;
-      case 99:
-        halt();
-        break;
-      default:
-        throw new Error(`OPCODE: ${program[instructionPointer]}`);
-    }
-  }
-
-  return lastOutput;
-
-  // General utility
-
-  function get(value: number, mode: number): number {
-    switch (mode) {
-      case 0:
-        return program[program[value]];
-      default:
-      case 1:
-        return program[value];
-    }
-  }
-
-  function put(loc: number, value: number): void {
-    program[program[loc]] = value;
-  }
-
-  // OPcode implementations
-
-  // 01: add
-  function add(modes: number[]): void {
-    const a = get(instructionPointer + 1, modes[0]);
-    const b = get(instructionPointer + 2, modes[1]);
-    put(instructionPointer + 3, a + b);
-
-    instructionPointer += 4;
-  }
-
-  // 02: multiply
-  function multiply(modes: number[]): void {
-    const a = get(instructionPointer + 1, modes[0]);
-    const b = get(instructionPointer + 2, modes[1]);
-    put(instructionPointer + 3, a * b);
-
-    instructionPointer += 4;
-  }
-
-  // 03: input
-  function input(): void {
-    put(instructionPointer + 1, inputGenerator.next().value);
-
-    instructionPointer += 2;
-  }
-
-  // 04: output
-  function output(modes: number[]): void {
-    if (lastOutput !== 0) {
-      throw new Error(
-        `Something went wrong! Instruction: ${instructionPointer}`
-      );
-    }
-    const output = get(instructionPointer + 1, modes[0]);
-    lastOutput = output;
-
-    instructionPointer += 2;
-  }
-
-  // 05: jump-if-true
-  // 06: jump-if-false
-  function jumpIf(req: boolean, modes: number[]): void {
-    if ((get(instructionPointer + 1, modes[0]) !== 0) === req) {
-      instructionPointer = get(instructionPointer + 2, modes[1]);
-    } else {
-      instructionPointer += 3;
-    }
-  }
-
-  // 07: less-than
-  // 08: equals
-  function check(
-    checker: (a: number, b: number) => boolean,
-    modes: number[]
-  ): void {
-    const a = get(instructionPointer + 1, modes[0]);
-    const b = get(instructionPointer + 2, modes[1]);
-    if (checker(a, b)) {
-      put(instructionPointer + 3, 1);
-    } else {
-      put(instructionPointer + 3, 0);
-    }
-
-    instructionPointer += 4;
-  }
-
-  // 99: halt
-  function halt(): void {
-    instructionPointer = -1;
-  }
-}
-
-// main(input, 5);
 
 function generateSettings(length: number): number[][] {
   return generateSettingsRec(new Array(length).fill(-1), 0, (new Array(length)).fill(0).map((val,index)=> index));
@@ -164,17 +26,15 @@ function generateSettingsRec(acc: number[], curr: number, openLocs: number[]): n
 const combinations = generateSettings(5);
 let currentBest = 0;
 for (const combination of combinations) {
-  let nextInput = 0;
-  function* generateInput(phases: number[]): Generator<number, number, void> {
-    for(let i = 0; i < phases.length; i++) {
-      yield phases[i];
-      yield nextInput;
+  let nextInput: number = 0;
+  for(let i = 0; i < combination.length; i++) {
+    const runner = intcode(input);
+    runner.next();
+    runner.next(combination[i]);
+    const result = runner.next(nextInput);
+    if(!result.done && result.value !== undefined) {
+      nextInput = result.value;
     }
-    return 0;
-  }
-  const gen = generateInput(combination);
-  for (let i = 0; i < combination.length; i++) {
-    nextInput = run([...input], gen);
   }
   if (nextInput > currentBest) {
     currentBest = nextInput;
--- src/7/2.ts
+++ src/7/2.ts
@@ -1,132 +1,11 @@
 import * as fs from "fs";
+import { intcode } from "../shared/intcode";
 
 const input = fs
   .readFileSync("input", "utf8")
   .split('\n')[0]
   .split(",")
   .map(Number);
-
-function* run(programBase: number[], name: number): Generator<number | undefined, undefined, number> {
-  const program = [...programBase];
-  let instructionPointer: number = 0;
-
-  while (instructionPointer >= 0) {
-    const operation = program[instructionPointer] % 100;
-    const modes = String(Math.floor(program[instructionPointer] / 100))
-      .split("")
-      .reverse()
-      .map(Number)
-      .concat(0, 0, 0, 0);
-
-    switch (operation) {
-      case 1:
-        add(modes);
-        break;
-      case 2:
-        multiply(modes);
-        break;
-      case 3: {
-        const input = yield undefined;
-        put(instructionPointer + 1, input);
-        instructionPointer += 2;
-        break;
-      }
-      case 4: {
-        const output = get(instructionPointer + 1, modes[0]);
-        yield output;
-        instructionPointer += 2;
-        break;
-      }
-      case 5:
-        jumpIf(true, modes);
-        break;
-      case 6:
-        jumpIf(false, modes);
-        break;
-      case 7:
-        check((a, b) => a < b, modes);
-        break;
-      case 8:
-        check((a, b) => a === b, modes);
-        break;
-      case 99:
-        halt();
-        break;
-      default:
-        throw new Error(`OPCODE: ${program[instructionPointer]}`);
-    }
-  }
-
-  return undefined;
-
-  // General utility
-
-  function get(value: number, mode: number): number {
-    switch (mode) {
-      case 0:
-        return program[program[value]];
-      default:
-      case 1:
-        return program[value];
-    }
-  }
-
-  function put(loc: number, value: number): void {
-    program[program[loc]] = value;
-  }
-
-  // OPcode implementations
-
-  // 01: add
-  function add(modes: number[]): void {
-    const a = get(instructionPointer + 1, modes[0]);
-    const b = get(instructionPointer + 2, modes[1]);
-    put(instructionPointer + 3, a + b);
-
-    instructionPointer += 4;
-  }
-
-  // 02: multiply
-  function multiply(modes: number[]): void {
-    const a = get(instructionPointer + 1, modes[0]);
-    const b = get(instructionPointer + 2, modes[1]);
-    put(instructionPointer + 3, a * b);
-
-    instructionPointer += 4;
-  }
-
-  // 05: jump-if-true
-  // 06: jump-if-false
-  function jumpIf(req: boolean, modes: number[]): void {
-    if ((get(instructionPointer + 1, modes[0]) !== 0) === req) {
-      instructionPointer = get(instructionPointer + 2, modes[1]);
-    } else {
-      instructionPointer += 3;
-    }
-  }
-
-  // 07: less-than
-  // 08: equals
-  function check(
-    checker: (a: number, b: number) => boolean,
-    modes: number[]
-  ): void {
-    const a = get(instructionPointer + 1, modes[0]);
-    const b = get(instructionPointer + 2, modes[1]);
-    if (checker(a, b)) {
-      put(instructionPointer + 3, 1);
-    } else {
-      put(instructionPointer + 3, 0);
-    }
-
-    instructionPointer += 4;
-  }
-
-  // 99: halt
-  function halt(): void {
-    instructionPointer = -1;
-  }
-}
 
 function generateSettings(length: number, start: number): number[][] {
   return generateSettingsRec(new Array(length).fill(-1), start, (new Array(length)).fill(0).map((val,index)=> index));
@@ -151,15 +30,9 @@ let currentBest = 0;
 for (const combination of combinations) {
   const amps = [];
   for (let i = 0; i < 5; i++) {
-    const amp = run([...input], i);
+    const amp = intcode([...input]);
     let shouldBeUndef = amp.next();
-    if (shouldBeUndef.value !== undefined) {
-      throw new Error('STARTUP INCORRECT');
-    }
     shouldBeUndef = amp.next(combination[i]);
-    if (shouldBeUndef.value !== undefined) {
-      throw new Error('STARTUP INCORRECT');
-    }
     amps.push(amp);
   }
   let currentInput = 0;
