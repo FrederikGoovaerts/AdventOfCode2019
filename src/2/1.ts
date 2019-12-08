8f32a751682275950aa1af58c47ca69643bf00b8
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2019-12-08 01:02:59 +0100

    Extract intcode processor and rewrite day 2 tests

--- src/2/1.ts
+++ src/2/1.ts
@@ -1,35 +1,18 @@
 import * as fs from "fs";
+import { intcode } from "../shared/intcode";
 
-// Read input
 const program: number[] = fs
   .readFileSync("input", "utf8")
   .split("\n")[0]
   .split(",")
   .map((n: string) => Number(n));
 
-let position = 0;
-let instruction = program[position];
-
 program[1] = 12;
-program[2] = 2;
+program[2] = 2
 
-const getValueIndirect = (num: number) => program[program[num]];
-
-while (instruction !== 99) {
-  if (instruction === 1) {
-    const location = program[position + 3];
-    const value =
-      getValueIndirect(position + 1) + getValueIndirect(position + 2);
-    program[location] = value;
-  } else if (instruction === 2) {
-    const location = program[position + 3];
-    const value =
-      getValueIndirect(position + 1) * getValueIndirect(position + 2);
-    program[location] = value;
-  }
-
-  position += 4;
-  instruction = program[position];
+const runner = intcode(program);
+const result = runner.next();
+if (result.done) {
+  console.log(result.value[0]);
 }
 
-console.log(program[0]);
--- src/2/2.ts
+++ src/2/2.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
+import { intcode } from "../shared/intcode";
 
-// Read input
-const programBase: number[] = fs
+const program: number[] = fs
   .readFileSync("input", "utf8")
   .split("\n")[0]
   .split(",")
@@ -9,34 +9,12 @@ const programBase: number[] = fs
 
 for (let i = 0; i < 100; i++) {
   for (let j = 0; j < 100; j++) {
-    const program = [...programBase];
-
-    let position = 0;
-    let instruction = program[position];
-
     program[1] = i;
     program[2] = j;
 
-    const getValueIndirect = (num: number) => program[program[num]];
-
-    while (instruction !== 99) {
-      if (instruction === 1) {
-        const location = program[position + 3];
-        const value =
-          getValueIndirect(position + 1) + getValueIndirect(position + 2);
-        program[location] = value;
-      } else if (instruction === 2) {
-        const location = program[position + 3];
-        const value =
-          getValueIndirect(position + 1) * getValueIndirect(position + 2);
-        program[location] = value;
-      }
-
-      position += 4;
-      instruction = program[position];
-    }
-
-    if (program[0] === 19690720) {
+    const runner = intcode(program);
+    const result = runner.next();
+    if (result.done && result.value[0] === 19690720) {
       console.log(i * 100 + j);
     }
   }
