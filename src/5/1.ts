8bdf7127551ffd5600cada05ad437a15a60eb170
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2019-12-09 08:50:11 +0100

    Reformat

--- src/5/1.ts
+++ src/5/1.ts
@@ -3,14 +3,14 @@ import { intcode } from "../shared/intcode";
 
 const input = fs
   .readFileSync("input", "utf8")
-  .split('\n')[0]
+  .split("\n")[0]
   .split(",")
   .map(Number);
 
 const runner = intcode(input);
 runner.next(1);
 let result = runner.next(1).value;
-while(result === 0) {
+while (result === 0) {
   result = runner.next(1).value;
 }
 console.log(result);
