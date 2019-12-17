e446c71f3a2855044b2f12cad1facd5288da3bc7
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2019-12-17 07:26:41 +0100

    Add solution to day 17 part 2

--- src/17/2.ts
+++ src/17/2.ts
@@ -9,15 +9,44 @@ const input = fs
   .map(Number);
 
 // Path to take:
-// L,6,R,8,L,4,R,8,L,12,
-// L,12,R,10,L,4,L,12,R,10,L,4,
-// L,12,L,6,
-// L,4,L,4,
-// L,12,R,10,L,4,L,12,
-// L,6,L,4,
-// L,4,L,12,R,10,L,4,
-// L,12,L,6,
-// L,4,L,4,
-// L,6,R8,L,4,R,8,L,12,L,6,R,8,L,4,R,8,L,12
+// A: L,6,R,8,L,4,R,8,L,12,
+// B: L,12,R,10,L,4,
+// B: L,12,R,10,L,4,
+// C: L,12,L,6,L,4,L,4,
+// B: L,12,R,10,L,4,
+// C: L,12,L,6,L,4,L,4,
+// B: L,12,R,10,L,4,
+// C: L,12,L,6,L,4,L,4,
+// A: L,6,R,8,L,4,R,8,L,12,
+// A: L,6,R,8,L,4,R,8,L,12,
+
+input[0] = 2;
+const main = "A,B,B,C,B,C,B,C,A,A\n".split("").map(val => val.charCodeAt(0));
+const a = "L,6,R,8,L,4,R,8,L,12\n".split("").map(val => val.charCodeAt(0));
+const b = "L,12,R,10,L,4\n".split("").map(val => val.charCodeAt(0));
+const c = "L,12,L,6,L,4,L,4\n".split("").map(val => val.charCodeAt(0));
+const video = "n\n".split("").map(val => val.charCodeAt(0));
 
 const runner = intcode(input);
+let next = runner.next();
+while (next.value.type === "OUTPUT") {
+  next = runner.next();
+}
+provide(main);
+provide(a);
+provide(b);
+provide(c);
+provide(video);
+
+function provide(input: number[]) {
+  let next = undefined;
+  for (const el of input) {
+    next = runner.next(el);
+    while (next.value.type === "OUTPUT") {
+      if (next.value.output > 255) {
+        console.log(next.value.output);
+      }
+      next = runner.next();
+    }
+  }
+}
