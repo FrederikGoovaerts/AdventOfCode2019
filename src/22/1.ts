946776a867e81e0eccde980917c23c9291109a46
Frederik Goovaerts <frederik.goovaerts@theoplayer.com>
2019-12-22 23:19:26 +0100

    Simplify 22.1

--- src/22/1.ts
+++ src/22/1.ts
@@ -1,29 +1,32 @@
 import * as fs from "fs";
+import * as assert from "assert";
 
 type Deal = { type: "NEW" | "INC" | "CUT"; value: number };
 
-const input: Deal[] = fs
-  .readFileSync("input", "utf8")
-  .split("\n")
-  .map(val => interpret(val))
-  .filter(val => val !== undefined) as Deal[];
+function indexOfCard(
+  inputFile: string,
+  value: number,
+  deckSize: number
+): number {
+  const input: Deal[] = fs
+    .readFileSync(inputFile, "utf8")
+    .split("\n")
+    .map(val => interpret(val))
+    .filter(val => val !== undefined) as Deal[];
 
-let tempDeck = new Array(10007);
-tempDeck.fill(0);
-let deck = tempDeck.map((val, ind) => ind);
-
-for (const op of input) {
-  if (op.type === "NEW") {
-    deck = newStack(deck);
-  } else if (op.type === "CUT") {
-    deck = cut(deck, op.value);
-  } else if (op.type === "INC") {
-    deck = inc(deck, op.value);
+  let state = value;
+  for (const op of input) {
+    if (op.type === "NEW") {
+      state = stack(state, deckSize);
+    } else if (op.type === "CUT") {
+      state = cut(state, op.value, deckSize);
+    } else if (op.type === "INC") {
+      state = inc(state, op.value, deckSize);
+    }
   }
+  return state;
 }
-console.log(deck.indexOf(2019));
 
-// helpers
 function interpret(input: string): Deal | undefined {
   if (input.includes("deal into new")) {
     return { type: "NEW", value: 0 };
@@ -35,21 +38,20 @@ function interpret(input: string): Deal | undefined {
   return undefined;
 }
 
-function newStack(deck: number[], val = 0): number[] {
-  return deck.reverse();
+function stack(val: number, deckSize: number): number {
+  return deckSize - 1 - val;
 }
 
-function cut(deck: number[], cutVal: number): number[] {
-  let cutValMod = cutVal < 0 ? cutVal + deck.length : cutVal;
-  return deck.slice(cutValMod, deck.length).concat(deck.slice(0, cutValMod));
+function cut(val: number, cutVal: number, deckSize: number): number {
+  return posMod(val - cutVal, deckSize);
 }
 
-function inc(deck: number[], incVal: number): number[] {
-  const newDeck = [];
-  let incCount = 0;
-  for (const el of deck) {
-    newDeck[incCount] = el;
-    incCount = (incCount + incVal) % deck.length;
-  }
-  return newDeck;
+function inc(val: number, incVal: number, deckSize: number): number {
+  return posMod(val * incVal, deckSize);
 }
+
+function posMod(a: number, m: number): number {
+  return (a + Math.ceil(Math.abs(a) / m) * m) % m;
+}
+
+console.log(indexOfCard("input", 2019, 10007));
